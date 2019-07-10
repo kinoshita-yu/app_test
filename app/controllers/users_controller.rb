19d31faac6ba16ebb8a0dd753f952e438a2bcbf5
ykinoshita <ykinoshita@mx.itmedia.co.jp>
2019-07-10 18:55:21 +0900

    登録、更新、削除の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,48 @@
 class UsersController < ApplicationController
+  #TODO IOをJSONにする
+
+  def index
+    user_list = User.all
+    render.json(status:"success", data:user_list)
+  end
+
+  def show
+    user = User.find(params[:id])
+
+    if user.blank?
+      render.json(status:"not found", data:[])
+      return;
+    end
+
+    render.json(status:"success", data:user)
+    return;
+
+  end
+
+  def create
+    if User.create!(name: params[:name])
+      render.json(status:"success", data:user)
+    else
+      render.json(status:"error", data:[])
+    end
+    return
+  end
+
+  def update
+    if User.find(:id).update!(name: params[:name])
+      render.json(status:"success", data:user)
+    else
+      render.json(status:"error", data:[])
+    end
+    return
+  end
+
+  def destroy
+    if User.destroy!(id: params[:id])
+      render.json(status:"success", data:[])
+    else
+      render.json(status:"error", data:[])
+    end
+    return
+  end
 end
